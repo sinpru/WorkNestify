f20d59813db454f2b891f18f804f107aa9ca1796
sinpru <thienthw07@gmail.com>
2025-02-28 16:47:05 +0700

    Added JobSeeker JobController, added homepage background

---
+++ WorkNestify.Web/Areas/JobSeeker/Controllers/JobController.cs
@@ -0,0 +1,199 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WorkNestify.DataAccess.Data;
+using WorkNestify.DataAccess.Entities.Jobs;
+using WorkNestify.DataAccess.Repositories.Interfaces;
+
+namespace WorkNestify.Web.Areas.JobSeeker.Controllers
+{
+    [Area("JobSeeker")]
+    public class JobController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public JobController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: JobSeeker/Job
+        public async Task<IActionResult> Index()
+        {
+            var jobs = await _unitOfWork.Jobs
+                .GetAllAsync(includeProperties: "Company,JobCategory,JobLevel,JobStatus,JobType");
+            return View();
+        }
+
+        // GET: JobSeeker/Job/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var job = await _unitOfWork.Jobs
+                .GetAsync(j => j.Id == id,
+                    includeProperties: "Company,JobCategory,JobLevel,JobStatus,JobType");
+            
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return View(job);
+        }
+
+        // GET: JobSeeker/Job/Create
+        public IActionResult Create()
+        {
+            PopulateDropdowns();
+            return View();
+        }
+
+        // POST: JobSeeker/Job/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,Location,Salary,StartDate,EndDate,CreatedDate,ModifiedDate,JobTypeId,JobStatusId,JobLevelId,JobCategoryId,CompanyId")] Job job)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.Jobs.AddAsync(job);
+                await _unitOfWork.SaveAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            
+            PopulateDropdowns();
+            return View(job);
+        }
+
+        // GET: JobSeeker/Job/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var job = await _unitOfWork.Jobs
+                .GetAsync(j => j.Id == id,
+                    includeProperties: "Company,JobCategory,JobLevel,JobStatus,JobType");
+            
+            if (job == null)
+            {
+                return NotFound();
+            }
+            
+            PopulateDropdowns();
+            return View(job);
+        }
+
+        // POST: JobSeeker/Job/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Location,Salary,StartDate,EndDate,CreatedDate,ModifiedDate,JobTypeId,JobStatusId,JobLevelId,JobCategoryId,CompanyId")] Job job)
+        {
+            if (id != job.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _unitOfWork.Jobs.UpdateAsync(job);
+                    await _unitOfWork.SaveAsync();
+
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await JobExists(job.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            
+            PopulateDropdowns();
+            return View(job);
+        }
+
+        // GET: JobSeeker/Job/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var job = await _unitOfWork.Jobs
+                .GetAsync(j => j.Id == id,
+                    includeProperties: "Company,JobCategory,JobLevel,JobStatus,JobType");
+            
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return View(job);
+        }
+
+        // POST: JobSeeker/Job/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var job = await _unitOfWork.Jobs
+                .GetAsync(j => j.Id == id,
+                    includeProperties: "Company,JobCategory,JobLevel,JobStatus,JobType");
+            
+            if (job != null)
+            {
+                _unitOfWork.Jobs.Remove(job);
+                await _unitOfWork.SaveAsync();
+            }
+            
+            return RedirectToAction(nameof(Index));
+        }
+        
+        // GET: JobSeeker/Job/Search?query=developer
+        public async Task<IActionResult> Search(string? query)
+        {
+            var jobs = await _unitOfWork.Jobs
+                .GetAllAsync(j => string.IsNullOrEmpty(query) || 
+                                  j.Title.Contains(query) ||
+                                  j.Description.Contains(query));
+            
+            return View(jobs);
+        }
+
+        private async Task<bool> JobExists(int id)
+        {
+            return await _unitOfWork.Jobs.GetAsync(j => j.Id == id) != null;
+        }
+        
+        private void PopulateDropdowns(Job job = null)
+        {
+            ViewData["CompanyId"] = new SelectList(_unitOfWork.Companies.GetAllAsync().Result, "Id", "Address", job?.CompanyId);
+            ViewData["JobCategoryId"] = new SelectList(_unitOfWork.JobCategories.GetAllAsync().Result, "Id", "Name", job?.JobCategoryId);
+            ViewData["JobLevelId"] = new SelectList(_unitOfWork.JobLevels.GetAllAsync().Result, "Id", "Name", job?.JobLevelId);
+            ViewData["JobStatusId"] = new SelectList(_unitOfWork.JobStatuses.GetAllAsync().Result, "Id", "Name", job?.JobStatusId);
+            ViewData["JobTypeId"] = new SelectList(_unitOfWork.JobTypes.GetAllAsync().Result, "Id", "Name", job?.JobTypeId);
+        }
+    }
+}
