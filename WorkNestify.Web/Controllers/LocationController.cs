39a7255cfe137f5a81eab149e6734afc06a2c520
sinpru <thienthw07@gmail.com>
2025-03-03 01:16:29 +0700

    Added Location API Controller

---
+++ WorkNestify.Web/Controllers/LocationController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorkNestify.Utilities.Services;
+
+namespace WorkNestify.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private readonly GhnService _ghnService;
+
+        public LocationController(GhnService ghnService)
+        {
+            _ghnService = ghnService;
+        }
+
+        [HttpGet("districts/{provinceId}")]
+        public async Task<IActionResult> GetDistricts(int provinceId)
+        {
+            var districts = await _ghnService.GetDistrictsAsync(provinceId);
+            return Ok(districts);
+        }
+
+        [HttpGet("wards/{districtId}")]
+        public async Task<IActionResult> GetWards(int districtId)
+        {
+            var wards = await _ghnService.GetWardsAsync(districtId);
+            return Ok(wards);
+        }
+    }
+}
